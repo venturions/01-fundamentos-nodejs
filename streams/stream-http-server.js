a3047db722fad8a6ca95f7493abfc52f5a5d0f59
venturions <alex.eduardo10@hotmail.com>
2023-10-03 19:44:57 -0300

    Criando middleware de JSON

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -1,15 +1,4 @@
 import http from 'node:http'
-import { Transform } from 'node:stream'
-
-class InverseNumberStream extends Transform {
-    _transform(chunk, encoding, callback) {
-        const transformed = Number(chunk.toString()) * -1
-
-        console.log(transformed)
-
-        callback(null, Buffer.from(String(transformed)))
-    }
-}
 
 const server = http.createServer(async (req, res) => {
     const buffers = []
