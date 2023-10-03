6a091e19ce6fa8a47cf4f92d8e107d89b1af353f
venturions <alex.eduardo10@hotmail.com>
2023-10-03 13:45:21 -0300

    Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 import http from 'node:http'
 
-
+const users = []
 
 const server = http.createServer((req, res) => {
     const { method, url } = req
 
     if (method === 'GET' && url === '/users') {
-        return res.end('Listagem de usuários')
+        return res.setHeader('Content-type', 'aplication/json').end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe', email: 'johndoe@example.com'
+        })
         return res.end('Criação de usuário')
     }
 
