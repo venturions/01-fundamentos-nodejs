885cf78c78ccc4511d45d54c0a3675617e1a08b4
venturions <alex.eduardo10@hotmail.com>
2023-10-04 15:30:50 -0300

    Rotas com parâmetros (Regex)

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,38 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
-export const routes = [{
-    method: 'GET',
-    path: '/users',
-    handler: (req, res) => {
-        const users = database.select('users')
+export const routes = [
+    {
+        method: 'GET',
+        path: buildRoutePath('/users'),
+        handler: (req, res) => {
+            const users = database.select('users')
 
-        return res.end(JSON.stringify(users))
-    }
-}, {
-    method: 'POST',
-    path: '/users',
-    handler: (req, res) => {
-        const { name, email } = req.body
-
-        const user = {
-            id: randomUUID(),
-            name,
-            email,
+            return res.end(JSON.stringify(users))
         }
+    },
+    {
+        method: 'POST',
+        path: buildRoutePath('/users'),
+        handler: (req, res) => {
+            const { name, email } = req.body
 
-        database.insert('users', user)
+            const user = {
+                id: randomUUID(),
+                name,
+                email,
+            }
 
-        return res.writeHead(201).end()
-    }
-}]
+            database.insert('users', user)
+
+            return res.writeHead(201).end()
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => { return res.end() }
+    }]
